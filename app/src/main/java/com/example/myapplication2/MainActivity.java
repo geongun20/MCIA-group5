2a0b3eeeaa040fceca08ba9b338bf827b758e2a9
박은만 <silverman@bag-eunman-ui-MacBook-Air.local>
2019-11-03 20:30:28 +0900

    eunman

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.myapplication2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class WelcomeActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity {
     ImageView welcomeImage;
 
     @Override
@@ -18,7 +17,7 @@ public class WelcomeActivity extends AppCompatActivity {
         Handler timer = new Handler();
 
         // populate the layout defined in xml file
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_report);
 
         // get the text view instance
         welcomeImage = (ImageView)findViewById(R.id.welcomeImage);
@@ -27,7 +26,7 @@ public class WelcomeActivity extends AppCompatActivity {
             @Override
             public void run() {
                 // make a intent
-                Intent intent = new Intent(WelcomeActivity.this, InfoActivity.class);
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
                 startActivity(intent);
                 finish();
             }
---
+++ app/src/main/java/com/example/myapplication2/TimelineActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+        ImageButton buttonH = findViewById(R.id.imageButton3) ;
+        buttonH.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TimelineActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton buttonT = findViewById(R.id.imageButton4) ;
+        buttonT.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TimelineActivity.this, TimelineActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton buttonR = findViewById(R.id.imageButton6) ;
+        buttonR.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TimelineActivity.this, ReportActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton buttonS = findViewById(R.id.imageButton7) ;
+        buttonS.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TimelineActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
