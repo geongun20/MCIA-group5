df03799920d7f932df6aa8eb10b15f8be5745f77
hailey421 <bob070030@gmail.com>
2019-11-04 02:13:15 +0900

    fixed xml

--- app/src/main/java/com/example/myapplication2/HomeActivity.java
+++ app/src/main/java/com/example/myapplication2/HomeActivity.java
@@ -11,6 +11,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
