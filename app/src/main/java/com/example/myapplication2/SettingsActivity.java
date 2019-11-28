8f9b1711ab105752ed1a6a802d08357d77d077ef
박은만 <silverman@bag-eunman-ui-MacBook-Air.local>
2019-11-28 11:26:24 +0900

    silverman edit

--- app/src/main/java/com/example/myapplication2/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication2/SettingsActivity.java
@@ -22,7 +22,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_settings);
         Gloal_Variable global = (Gloal_Variable) getApplication();
 
 
