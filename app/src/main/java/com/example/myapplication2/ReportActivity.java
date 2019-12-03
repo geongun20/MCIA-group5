45dbb3aa8ad794bbbfca23156830c88c0529a43b
kyungwon-lee <kw3753@naver.com>
2019-12-03 13:20:20 +0900

    kyungwon edit

--- app/src/main/java/com/example/myapplication2/ReportActivity.java
+++ app/src/main/java/com/example/myapplication2/ReportActivity.java
@@ -1,7 +1,13 @@
 package com.example.myapplication2;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -53,8 +59,23 @@ public class ReportActivity extends AppCompatActivity{
         input.readFile("sample_data.txt", getApplicationContext());
         List<String> list = input.getData()[input.getMonth()][input.getDay()];
 
-        TextView msg = findViewById(R.id.textYoucould);
-        msg.setText(String.format("You could have lived \n %f more minutes",((double)input.countThisWeek())*(13.8)));
+        TextView report_main = findViewById(R.id.report_main_text);
+        String content = report_main.getText().toString();
+        SpannableString spannableString = new SpannableString(content);
+
+        String word = "3 DAYS";
+        int start = content.indexOf(word);
+        int end = start + word.length();
+
+        spannableString.setSpan(new RelativeSizeSpan(2f), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#26689A")), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+        report_main.setText(spannableString);
+//        DecimalFormat form = new DecimalFormat(("#.#"));
+//        TextView msg = findViewById(R.id.textYoucould);
+//        msg.setText(String.format("You could have lived \n %s more minutes",(form.format((double)input.countThisWeek()*(13.8)))));
 
     }
 }
